463322fa5a91440c57f68def658187b4f76c0f42
Julie of the Wolves <juliana.novic@gmail.com>
2018-12-04 20:08:31 -0500

    changing directory

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from sqlalchemy.sql import label
 
-from flask import Flask, jsonify, render_template, redirect
+from flask import Flask, jsonify, render_template, redirect, request, send_from_directory
 
 #################################################
 # Database Setup
@@ -12,15 +12,19 @@ from flask import Flask, jsonify, render_template, redirect
 
 
 #Flask set up
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='')
 
-@app.route("/")
-def index():
-    return (
-        f"Available Routes:<br/>"
-        f"/data<br/>"
-    )
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
+#@app.route('/static/jss/<path>')
+#def send_js(path):
+#    return send_from_directory('/static/jss', path)
+
+#@app.route('/static/css/<path>')
+#def send_css(path):
+#    return send_from_directory('/static/css', path)
 
 @app.route("/data")
 def data():
