14e38998e34d6b8de99a754076d3b2260456914e
iris <ikleinman19@gmail.com>
2018-12-04 20:18:58 -0500

    Map

--- static/jss/map.js
+++ static/jss/map.js
@@ -1,25 +1,25 @@
 
-function createMap(movie_locations) {
-
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.light",
-  accessToken: API_KEY
-});
-// Create a baseMaps object to hold the lightmap layer
-var baseMaps = {
-  "Light Map": lightmap
-};
-// Create an overlayMaps object to hold the bikeStations layer
-var overlayMaps = {
-  "Movie Locations": movie_locations
-};
-
-// Create the map object with options
 var map = L.map("map-id", {
   center: [40.73, -74.0059],
-  zoom: 12,
-  layers: [lightmap, movie_locations]
+  zoom: 150,
+  //layers: [lightmap, movie_locations]
 });
-}
+
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+  maxZoom: 1000,
+  id: "mapbox.light",
+  accessToken: API_KEY
+}).addTo(map);
+// Create a baseMaps object to hold the lightmap layer
+//var baseMaps = {
+  //"Light Map": lightmap
+//};
+// Create an overlayMaps object to hold the bikeStations layer
+//var overlayMaps = {
+  //"Movie Locations": movie_locations
+//};
+
+// Create the map object with options
+
+//}
