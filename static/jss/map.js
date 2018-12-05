1b90f026add6dfd8f1a510e156d9c1fd884bb9fd
iris <ikleinman19@gmail.com>
2018-12-05 11:33:17 -0500

    Not sure what change I made but I may as well commit it

--- static/jss/map.js
+++ static/jss/map.js
@@ -1,13 +1,13 @@
 
 var map = L.map("map-id", {
-  center: [40.73, -74.0059],
-  zoom: 150,
+  center: [15.3265, -76.157],
+  zoom: 2,
   //layers: [lightmap, movie_locations]
 });
 
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-  maxZoom: 1000,
+  maxZoom: 18,
   id: "mapbox.light",
   accessToken: API_KEY
 }).addTo(map);
