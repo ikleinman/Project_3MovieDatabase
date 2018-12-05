58e45625fb2b0f39a0f68ceb06ea5405e305c6ab
Julie of the Wolves <juliana.novic@gmail.com>
2018-12-04 19:37:27 -0500

    more flask edits

--- app.py
+++ app.py
@@ -9,35 +9,33 @@ from flask import Flask, jsonify, render_template, redirect
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///movie_data.sqlite")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-Movies = Base.classes.movies
-
-# Create our session (link) from Python to the DB
-session = Session(engine)
 
 #Flask set up
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-  return (
+    return (
         f"Available Routes:<br/>"
-        f"/map<br/>"
-        f"/graphs<br/>"
-        f"/directors<br/>"
-    
+        f"/data<br/>"
     )
 
-@app.route("/map")
-def map():
+
+@app.route("/data")
+def data():
+    engine = create_engine("sqlite:///movie_data.sqlite")
+
+# reflect an existing database into a new model
+    Base = automap_base()
+# reflect the tables
+    Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+    Movies = Base.classes.movies
+
+# Create our session (link) from Python to the DB
+    session = Session(engine)
     sel1=[Movies.movie_title, Movies.director_name, Movies.gross, Movies.genres, Movies.country, Movies.content_rating, Movies.budget, Movies.title_year, Movies.Longitude, Movies.Latitude]
     country_data = session.query(*sel1).all()
     data1 = []
@@ -56,44 +54,7 @@ def map():
     return jsonify(data1)
    
 
-@app.route("/graphs")
-def graphs():
-   
-    sel2=[Movies.movie_title, Movies.director_name, Movies.gross, Movies.genres, Movies.country, Movies.content_rating, Movies.budget, Movies.title_year, Movies.Longitude, Movies.Latitude]
-    graph_data = session.query(*sel2).all()
-    data2 = []
-    for d in graph_data:
-        data2.append({
-               "movie_title":d[0],
-                "director_name":d[1],
-                "gross":d[2],
-                "genres":d[3],
-                "country":d[4],
-                "content_rating":d[5],
-                "budget":d[6],
-                "title_year":d[7],
-                "Latitude":d[8],
-                "Longitude":d[9]})
-    return jsonify(data2)
 
-@app.route("/directors")
-def directors():
-    sel3=[Movies.movie_title, Movies.director_name, Movies.gross, Movies.genres, Movies.country, Movies.content_rating, Movies.budget, Movies.title_year, Movies.Longitude, Movies.Latitude]
-    director_data = session.query(*sel3).all()
-    data3 = []
-    for d in director_data:
-        data3.append({
-               "movie_title":d[0],
-                "director_name":d[1],
-                "gross":d[2],
-                "genres":d[3],
-                "country":d[4],
-                "content_rating":d[5],
-                "budget":d[6],
-                "title_year":d[7],
-                "Latitude":d[8],
-                "Longitude":d[9]})
-    return jsonify(data3)
 
 if __name__ == '__main__':
     app.run(debug=True)
