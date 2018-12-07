4090ed762ddbfe3781e85e6c40ed876d87574016
Julie of the Wolves <juliana.novic@gmail.com>
2018-12-06 19:59:20 -0500

    d3 table

--- static/jss/director.js
+++ static/jss/director.js
@@ -1,59 +1,15 @@
-// Define SVG area dimensions
-var svgWidth = 960;
-var svgHeight = 660;
+var url = "http://localhost:5000/data"
+var table = d3.select("#director-table");
+var tablebody = table.select("tbody");
 
-// Define the chart's margins as an object
-var chartMargin = {
-  top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30
-};
+d3.json(url).then(function(data) {
+ console.log(data);
 
-// Define dimensions of the chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+data.forEach((item) => {
+  var tablerow = tablebody.append("tr");
+  Object.entries(item).forEach(([key,value]) => {
+      var tabledivision = tablerow.append("td");
+  tabledivision.text(value);})
+} ) });
 
-// Select body, append SVG area to it, and set the dimensions
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
-// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
-// to the margins set in the "chartMargin" object.
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-// Load data from movie_data.csv
-d3.csv("../movie_data.csv", function(error, movie_Data) {
-
-  // Log an error if one exists
-  if (error) return console.warn(error);
-
-  // Print the tvData
-  console.log(movie_data);
-
-  // Cast the hours value to a number for each piece of tvData
-  movie_data.forEach(function(data) {
-    data.hours = +data.hours;
-  });
-
-  var barSpacing = 10; // desired space between each bar
-  var scaleY = 10; // 10x scale on rect height
-
-  // @TODO
-  // Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
-  var barWidth = (chartWidth - (barSpacing * (movie_data.length - 1))) / movie_data.length;
-
-  // Create code to build the bar chart using the tvData.
-  chartGroup.selectAll(".bar")
-    .data(movie_data)
-    .enter()
-    .append("rect")
-    .classed("bar", true)
-    .attr("width", d => barWidth)
-    .attr("height", d => d.hours * scaleY)
-    .attr("x", (d, i) => i * (barWidth + barSpacing))
-    .attr("y", d => chartHeight - d.hours * scaleY);
-});
