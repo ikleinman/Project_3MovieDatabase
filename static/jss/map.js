935e3148cb15710e4d37ee4001026d49a2786cf0
iris <ikleinman19@gmail.com>
2018-12-06 19:26:21 -0500

    file path

--- static/jss/map.js
+++ static/jss/map.js
@@ -1,8 +1,10 @@
+function createMap(movie_locations){
+
 
 var map = L.map("map-id", {
   center: [15.3265, -76.157],
   zoom: 2,
-  //layers: [lightmap, movie_locations]
+  layers: [lightmap, movie_locations]
 });
 
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
@@ -12,14 +14,23 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/
   accessToken: API_KEY
 }).addTo(map);
 // Create a baseMaps object to hold the lightmap layer
-//var baseMaps = {
-  //"Light Map": lightmap
-//};
-// Create an overlayMaps object to hold the bikeStations layer
-//var overlayMaps = {
-  //"Movie Locations": movie_locations
-//};
+var baseMaps = {
+  "Light Map": lightmap
+};
+// Create an overlayMaps object to hold the movie locations layer
+var overlayMaps = {
+  "Movie Locations": movie_locations
+};
 
-// Create the map object with options
+// layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
+}
+const url = "http://127.0.0.1:5000/data";
+var data = []
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
 
-//}
+  console.log(data);
+});
