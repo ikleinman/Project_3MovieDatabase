7b5afab10bbc4d25e85277f4da293076f89d2c97
Julie of the Wolves <juliana.novic@gmail.com>
2018-12-07 21:17:24 -0500

    directors table phase 1

--- static/jss/director.js
+++ static/jss/director.js
@@ -1,15 +1,29 @@
 var url = "http://localhost:5000/data"
+var submit = d3.select("#filter-btn");
 var table = d3.select("#director-table");
 var tablebody = table.select("tbody");
 
-d3.json(url).then(function(data) {
- console.log(data);
 
-data.forEach((item) => {
-  var tablerow = tablebody.append("tr");
-  Object.entries(item).forEach(([key,value]) => {
-      var tabledivision = tablerow.append("td");
-  tabledivision.text(value);})
-} ) });
+dataTable = d3.json(url).then(function(d) {
+  
+ 
+ 
+   
+function filterDirectors(){
+  d3.event.preventDefault();
+  var inputElement =d3.select("#director_search");
+  var inputValue = inputElement.property("value");
+  var filteredData = d.filter(director=> director.director_name === inputValue);
+  filteredData.forEach((item) => {
+      var tablerow = tablebody.append("tr");
+      Object.entries(item).forEach(([key,value]) => {
+          var tabledivision = tablerow.append("td");
+      tabledivision.text(value);})  
+  });
+};
 
+submit.on("click", filterDirectors);
+});
+
+     
 
